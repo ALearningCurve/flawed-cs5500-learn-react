44c6fd198a8f6204bf35da2fbd59d71a534fa939
alearningcurve <will@willwalling.com>
2024-10-01 11:57:35 -0400

    9/26/2024 in class

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,50 +4,11 @@
 // import styles from '@/styles/Home.module.css'
 
 
-// slide "for you to do" #1
-// import Profile from "./qcomps/profile_mistake";
-// import MyComp from "./qcomps/firstcomp";
-// export default function Home() {
-//   return (
-//     <div>
-//       <Profile />
-//       <MyComp />
-//     </div>
-//   );
-// }
-
-
-// slide "for you to do" #2
-// import Bio from "./qcomps/bios";
-// import Todos from "./qcomps/todos";
-// export default function Home() {
-//   return (
-//     <div>
-//       <Bio />
-//       <Todos />
-//     </div>
-//   );
-// }
-
-
-// slide "for you to do" #3 - Component Props;
-// import Profile from "./components/profile_props";
-// import Gallery from "./qcomps/gallery_props";
-// export default function Home() {
-//   return (
-//     <div>
-//       {/* <Profile /> */}
-//       <Gallery />
-//     </div>
-//   );
-// }
-
-// slide "for you to do" #4 - container component/conditional rendering
-import PackingList from "./qcomps/props_item";
+import ListPlain from "./components/list_plain";
 export default function Home() {
   return (
     <div>
-      <PackingList />
+      <ListPlain />
     </div>
   );
 }
